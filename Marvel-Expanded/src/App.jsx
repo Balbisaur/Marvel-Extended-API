32bcaeb17fe237b0fad08ff5c546d5d6de5e59b0
Gerardo Ramirez <166074441+Balbisaur@users.noreply.github.com>
2024-06-30 10:55:13 -0500

    Update App.jsx

--- Marvel-Expanded/src/App.jsx
+++ Marvel-Expanded/src/App.jsx
@@ -1,31 +1,38 @@
-import React from 'react';
+import React, { lazy, Suspense } from 'react';
 import { Routes, Route, NavLink } from 'react-router-dom';
 import Home from './Components/Home';
-import BrowseCharacters from './Components/Characters';
-import CharacterDetails from './Components/CharacterDetail';
-import Comics from './Components/Comic';
 import NotFound from './Components/NotFound';
 import './App.css';
+
+const BrowseCharacters = lazy(() => import('./Components/Characters'));
+const CharacterDetails = lazy(() => import('./Components/CharacterDetail'));
+const Comics = lazy(() => import('./Components/Comic'));
 
 const App = () => {
   return (
     <div>
-        <ul>
-          <li>
-            <NavLink activeStyle={{ color:'#5754a8' }} to="/" className="nav-link">Home</NavLink>
-          </li>
-          <li>
-            <NavLink activeStyle={{ color:'#5754a8' }} to="/characters" className="nav-link">Browse Characters</NavLink>
-          </li>
-          <li>
-            <NavLink activeStyle={{ color:'#5754a8' }} to="/comics" className="nav-link">Comics</NavLink>
-          </li>
-        </ul>
+      <ul>
+        <li>
+          <NavLink exact activeStyle={{ color: '#5754a8' }} to="/" className="nav-link">
+            Home
+          </NavLink>
+        </li>
+        <li>
+          <NavLink exact activeStyle={{ color: '#5754a8' }} to="/characters" className="nav-link">
+            Browse Characters
+          </NavLink>
+        </li>
+        <li>
+          <NavLink exact activeStyle={{ color: '#5754a8' }} to="/comics" className="nav-link">
+            Comics
+          </NavLink>
+        </li>
+      </ul>
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/characters" element={<BrowseCharacters />} />
-        <Route path="/character/:characterId" element={<CharacterDetails />} />
-        <Route path="/comics" element={<Comics />} />
+        <Route path="/characters/*" element={<Suspense fallback={<div>Loading...</div>}><BrowseCharacters /></Suspense>} />
+        <Route path="/character/:characterId/*" element={<Suspense fallback={<div>Loading...</div>}><CharacterDetails /></Suspense>} />
+        <Route path="/comics/*" element={<Suspense fallback={<div>Loading...</div>}><Comics /></Suspense>} />
         <Route path="*" element={<NotFound />} />
       </Routes>
     </div>
@@ -33,3 +40,4 @@ const App = () => {
 };
 
 export default App;
+
